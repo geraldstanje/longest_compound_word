dacfa987c28f7fa4bd6ec04ee0bc26fcd9e2bc33
Gerald Stanje <Gerald.Stanje@gmail.com>
2016-01-27 16:02:45 +0100

    final commit

--- main.go
+++ main.go
@@ -1,36 +1,45 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "os"
-  "time"
+	"flag"
+	"fmt"
+	"lcwsolver"
+	"log"
+	"os"
+	"time"
 )
 
 var flagFilename string
 var flagBenchmark bool
 
 func init() {
-  flag.StringVar(&flagFilename, "f", "", "Path to word.list")
-  flag.BoolVar(&flagBenchmark, "b", false, "Benchmark")
-  flag.Parse()
+	flag.StringVar(&flagFilename, "f", "", "Path to word.list")
+	flag.BoolVar(&flagBenchmark, "b", false, "Benchmark")
+	flag.Parse()
 }
 
 func main() {
-  if len(flagFilename) == 0 {
-    fmt.Printf("Usage: %s -f filename\n", os.Args[0])
-    os.Exit(1)
-  }
+	if len(flagFilename) == 0 {
+		fmt.Printf("Usage: %s -f filename\n", os.Args[0])
+		os.Exit(1)
+	}
 
-  var start time.Time
-  if flagBenchmark {
-    start = time.Now()
-  }
+	var start time.Time
+	if flagBenchmark {
+		start = time.Now()
+	}
 
-  if flagBenchmark {
-    elapsed := time.Since(start)
-    fmt.Println("Runtime:", elapsed.Nanoseconds()/1000000, "ms")
-  }
+	s := lcwsolver.New()
+	err := s.LoadWordList(flagFilename)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	result := s.GetResult()
 
-  //fmt.Println("longest compound word:", result)
+	if flagBenchmark {
+		elapsed := time.Since(start)
+		fmt.Println("Runtime:", elapsed.Nanoseconds()/1000000, "ms")
+	}
+
+	fmt.Println("Longest compound word:", result)
 }
